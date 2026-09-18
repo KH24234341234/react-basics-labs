bad8c426a7cf7aa054ac790c024b4cbbd616e576
KH24234341234 <20107672@setu.ie>
2026-09-18 11:25:59 +0100

    Add task functionality

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import AddTaskForm from './components/Form';
 import React, { useState } from 'react';
 import './App.css';
@@ -26,9 +27,45 @@ function App() {
     setTaskState({tasks});
   } 
 
+    const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
+  });
 
+    const formChangeHandler = (event) => {
+    let form = {...formState};
 
+    switch(event.target.name) {
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+    console.log(formState);
+
+  }
+
+    const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
     
+    tasks.push(form);
+    setTaskState({tasks});
+  }
+
 
     return (
     <div className="container">
@@ -48,11 +85,17 @@ function App() {
       
     />
   ))} 
-        <AddTaskForm />
+          <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
+
+         
+
+
 
 
      
     </div>
+
+    
   );
 
 }
