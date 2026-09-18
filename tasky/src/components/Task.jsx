0c4e95867286c6e4e075d0dee0147aa1088839e6
KH24234341234 <20107672@setu.ie>
2026-09-18 11:09:55 +0100

    Mark Task as done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,13 +1,19 @@
 const Task = (props) => {
     
     return (
-        <div className="card">
+
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+
+            
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.children}</p>
             {/*new prop class created here, in same vein as title */}
             <p className ="description">{props.description}</p>  
             <p className ="priority">{props.priority}</p>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
+
+
         </div>
     )
 }
